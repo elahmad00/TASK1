733cedf3d7f995dde33420f57b6412eb3088e823
ahmad muhammad <elahmad00@gmail.com>
2022-10-27 19:25:39 +0100

    changes in the work

--- Task1/urls.py
+++ Task1/urls.py
@@ -17,6 +17,7 @@ from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path
 from task import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home',views.home, name= 'home'),
